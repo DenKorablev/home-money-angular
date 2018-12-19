edc903ba5f4269ca4c4bc61a41b654ac8d770856
DenKorablev <denis18051990@mail.ru>
2018-12-19 15:42:39 +0300

    Заливаю проект

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
+import { UsersService } from './shared/services/users.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './shared/services/auth.service';
+import { SystemModule } from './system/system.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AuthModule,
+    SystemModule,
+    AppRoutingModule
+  ],
+  providers: [
+    UsersService, 
+    AuthService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
