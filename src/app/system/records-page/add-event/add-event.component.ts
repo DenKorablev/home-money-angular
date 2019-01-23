61022aba651a5ea2f32a5578ef19e28b3ede7c75
DenKorablev <denis18051990@mail.ru>
2019-01-23 17:20:11 +0300

    Начал 22 урок

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Category } from '../../shared/models/category.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'wfm-add-event',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+  types = [
+    {type: 'income', label: 'Доход'},
+    {type: 'outcome', label: 'Расход'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+
+  }
 }
